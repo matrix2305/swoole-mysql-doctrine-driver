b61a1387a86b7e8be1849e7616e6a59483751676
matrix2305 <srki.rado@gmail.com>
2023-05-23 23:54:15 +0200

    Completed mysql driver

--- src/Driver.php
+++ src/Driver.php
@@ -2,112 +2,31 @@
 
 namespace Doctrine\DBAL\Driver\Swoole\Coroutine\Mysql;
 
+use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Driver\AbstractMySQLDriver;
-use Doctrine\DBAL\Driver\Connection as ConnectionInterface;
-use Doctrine\DBAL\Driver\Swoole\Coroutine\Mysql\Exception\ConnectionException;
-use Doctrine\DBAL\Driver\Swoole\Coroutine\Mysql\Exception\DriverException;
-use Doctrine\Deprecations\Deprecation;
-use PDO;
-use Swoole\ConnectionPool;
+use Doctrine\DBAL\Driver\ResultStatement;
 use Swoole\Coroutine;
-use Laravel\Octane\Facades\Octane;
+use Swoole\Coroutine\MySQL;
 
 final class Driver extends AbstractMySQLDriver
 {
-    public const DEFAULT_POOL_SIZE = 8;
-    private static ConnectionPool $pool;
-
-    public function connect(array $params, $username = null, $password = null, array $driverOptions = []): ConnectionInterface
+    public function connect(array $params, ?string $username = null, ?string $password = null, array $driverOptions = []): Connection
     {
-        if (!empty($driverOptions)) {
-            $params['driverOptions'] = $driverOptions;
-        }
-        if ($username) {
-            $params['user'] = $username;
-        }
-        if ($password) {
-            $params['password'] = $password;
-        }
+        $mysql = new MySQL();
+        $mysql->connect([
+            'host' => $params['host'],
+            'port' => $params['port'],
+            'user' => $username,
+            'password' => $password,
+            'database' => $params['dbname'],
+            // Add more connection options as needed
+        ]);
 
-        if (!isset(self::$pool)) {
-            [$pool] = Octane::concurrently([function () use($params) {
-                $pool = new ConnectionPool(
-                    fn(): Connection => $this->createConnection($this->dsn($params), $params),
-                    $params['poolSize'] ?? self::DEFAULT_POOL_SIZE,
-                );
-                return $pool;
-            }]);
-
-            self::$pool = $pool;
-        }
-
-        $connection = self::$pool->get();
-        defer(static fn() => self::$pool->put($connection));
-        return $connection;
-    }
-
-    /**
-     * @throws DriverException
-     * @throws ConnectionException
-     */
-    public function createConnection(string $dsn, array $params): Connection
-    {
-        $driverOptions = $params['driverOptions'] ?? [];
-
-        if (! empty($params['persistent'])) {
-            $driverOptions[PDO::ATTR_PERSISTENT] = true;
-        }
-
-        return new Connection(
-            $dsn,
-            $params['user'] ?? '',
-            $params['password'] ?? '',
-            $driverOptions
-        );
-    }
-
-    private function dsn(array $params): string
-    {
-        if (array_key_exists('url', $params)) {
-            return $params['url'];
-        }
-
-        $params['host'] ??= '127.0.0.1';
-        $params['port'] ??= 3306;
-        $params['dbname'] ??= 'mysql';
-
-        $dsn = 'mysql:';
-        if (isset($params['host']) && $params['host'] !== '') {
-            $dsn .= 'host=' . $params['host'] . ';';
-        }
-
-        if (isset($params['port'])) {
-            $dsn .= 'port=' . $params['port'] . ';';
-        }
-
-        if (isset($params['dbname'])) {
-            $dsn .= 'dbname=' . $params['dbname'] . ';';
-        }
-
-        if (isset($params['unix_socket'])) {
-            $dsn .= 'unix_socket=' . $params['unix_socket'] . ';';
-        }
-
-        if (isset($params['charset'])) {
-            $dsn .= 'charset=' . $params['charset'] . ';';
-        }
-
-        return $dsn;
+        return SwooleConnection($mysql);
     }
 
     public function getName()
     {
-        Deprecation::trigger(
-            'doctrine/dbal',
-            'https://github.com/doctrine/dbal/issues/3580',
-            'Driver::getName() is deprecated'
-        );
-
-        return 'mysqli';
+        // TODO: Implement getName() method.
     }
 }
